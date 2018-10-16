bcfc46e1eebe68d775e3f69e715931a085860f83
Andrew Lanza <andrewlanza@Andrews-MBP-2.localdomain>
2018-10-16 10:56:00 -0400

    this assignment is gorgeous, and complete

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react"
+
+class Footer extends Component {
+  state = {}
+  render() {
+    return (
+      <div>
+        <footer>
+          <p class="footer-logo">
+            <img src="https://octodex.github.com/ui/logo.png" />
+          </p>
+        </footer>
+      </div>
+    )
+  }
+}
+
+export default Footer
